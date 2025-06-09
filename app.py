d86f8ccd79a39022ba86ae77dfb60273ea6787d9
abhai-iitm <21f3001705@ds.study.iitm.ac.in>
2025-06-09 06:20:20 +0000

    question change

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-# main.py
-from fastapi import FastAPI
+#main.py
+from fastapi import FastAPI, Query
+from typing import List
+import json
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
         return {"message": "Hello, World!"}
+
+@app.get("/api/")
+def find_name(names: List[str] = Query(...)):
+    with open('name.json', mode='r') as file:
+        data = json.load(file)
+        return {"marks": [data.get(n) for n in names]]}
